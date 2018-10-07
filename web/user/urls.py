a4fc519371f8ecea6e47821bd830a65770252142
Pablo Ferreiro <pablo.ferre@gmail.com>
2018-10-07 20:08:08 -0300

    actualizando mac

--- web/user/urls.py
+++ web/user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tasaciones/', views.appraiserListOfAppraisals.as_view(), name='tasaciones-usuario'),
+    #path('tasaciones/', views.appraiserListOfAppraisals.as_view(), name='tasaciones-usuario'),
+    path('tasaciones/', views.userAppraisals, name='tasaciones-usuario'),
 ]
--- web/user/views.py
+++ web/user/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from appraisal.models import Appraisal
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import MultipleObjectsReturned
 
 
 class appraiserListOfAppraisals(LoginRequiredMixin, ListView):
@@ -15,18 +19,17 @@ class appraiserListOfAppraisals(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Appraisal.objects.filter(tasadorUser=self.request.user).filter(status=Appraisal.STATE_ACTIVE).order_by('timeCreated')
 
-'''
+
 @login_required(login_url='/')
-def main(request):
+def userAppraisals(request):
 
     appraisals_active = Appraisal.objects.filter(status=Appraisal.STATE_ACTIVE).order_by('timeCreated')
 
     appraisals_finished = Appraisal.objects.filter(status=Appraisal.STATE_FINISHED).order_by('timeCreated')
 
     context = {
-        'appraisals_active':appraisals_active,
-        'appraisals_finished':appraisals_finished}
+        'appraisals_active': appraisals_active,
+        'appraisals_finished': appraisals_finished}
+    return render(request, 'user/index.html', context)
 
-    return render(request, 'main/index.html',context)
-'''
 
