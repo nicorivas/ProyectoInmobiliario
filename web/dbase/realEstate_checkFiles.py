81cb7edecdea5b6fc0d931bf92ac01f26f572b41
Nicolas Rivas <nicorivas@gmail.com>
2018-10-24 14:30:58 -0300

    Moved output routines and put in a class. Fixed some things of the checkFiles script

--- web/dbase/realEstate_checkFiles.py
+++ web/dbase/realEstate_checkFiles.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 '''
-Script to check integrity and completness of realestate source data files
+Script to check integrity and completness of real estate source data files
+! Directories are hard coded
 '''
 import datetime
 from tools import *
 from globals import *
+from output import *
 import codecs
 
 # Importing DJANGO things
@@ -23,71 +25,72 @@ from apartment.models import Apartment
 from region.models import Region
 from commune.models import Commune
 
+datapath = REALSTATE_DATA_PATH+'/source'
+
 regions = Region.objects.filter(code=13) # por ahora solo R.M.
 communes = Commune.objects.all()
-datapath = REALSTATE_DATA_PATH
 
-log = open('log','w')
+out = messenger(open('logs/datafiles','w'))
 
 for region in regions:
-    basepath = datapath+'/source/{}/'.format(slugify(region.name))
+    basepath = datapath+'/{}/'.format(slugify(region.name))
     if not os.path.isdir(basepath):
-        warning('{} DIRECTORY DOES NOT EXIST'.format(region.name),log)
+        out.warning('{} DIRECTORY DOES NOT EXIST'.format(region.name))
     else:
-        say('{} DIR OK'.format(region.name))
+        out.say('{} DIR OK'.format(region.name))
 
     communes_region = communes.filter(region=region)
     for commune in communes_region:
-        basepath = datapath+'/source/{}/{}/'.format(
+        basepath = datapath+'/{}/{}/'.format(
             slugify(region.name),
             slugify(commune.name))
         if not os.path.isdir(basepath):
-            error('{} DIRECTORY NOT FOUND'.format(commune.name),log)
+            out.error('{} DIRECTORY NOT FOUND'.format(commune.name))
         else:
-            say('{} DIR OK'.format(commune.name))
+            out.say('{} DIR OK'.format(commune.name))
 
         sources = ['PortalInmobiliario']
 
         for source in sources:
             basepath = basepath+'/'+source
             if not os.path.isdir(basepath):
-                error('{} {} DIRECTORY NOT FOUND'.format(commune.name,source),log)
+                out.error('{} {} DIRECTORY NOT FOUND'.format(commune.name,source))
                 continue
 
-            say('{} {} OK'.format(commune.name,source))
+            out.say('{} {} OK'.format(commune.name,source))
 
             pfile = basepath+'/{}_urls_PI.json'.format(slugify(commune.name))
             if not os.path.isfile(pfile):
                 tmp = basepath+'/{}_urls_PI.json'.format(slugify(commune.name,only_ascii=True))
                 if not os.path.isfile(tmp):
-                    error('{} {} URLS FILE NOT FOUND'.format(commune.name,source),log)
+                    out.error('{} {} URLS FILE NOT FOUND'.format(commune.name,source))
                     continue
                 else:
-                    warning('{} {} URLS FILE FALTA TILDE?'.format(commune.name,source),log)
+                    out.warning('{} {} URLS FILE FALTA TILDE?'.format(commune.name,source))
                     pfile = tmp
 
             data = json.load(open(pfile))
-            say('{} {} URLS: {}'.format(commune.name,source,len(data)))
+            out.say('{} {} URLS: {}'.format(commune.name,source,len(data)))
 
             pfile = basepath+'/{}_properties_PI.json'.format(slugify(commune.name))
             if not os.path.isfile(pfile):
                 tmp = basepath+'/{}_properties_PI.json'.format(slugify(commune.name,only_ascii=True))
                 if not os.path.isfile(tmp):
-                    error('{} {} PROPERTIES FILE DOES NOT EXIST'.format(commune.name,source),log)
+                    out.error('{} {} PROPERTIES FILE DOES NOT EXIST'.format(commune.name,source))
                     continue
                 else:
-                    warning('{} {} PROPERTIES FILE FALTA TILDE?'.format(commune.name,source),log)
+                    out.warning('{} {} PROPERTIES FILE FALTA TILDE?'.format(commune.name,source))
                     pfile = tmp
 
             data = json.load(open(pfile))
-            say('{} {} PROPERTIES FILE PROPERTIES: {}'.format(commune.name,source,len(data)))
+            out.say('{} {} PROPERTIES FILE PROPERTIES: {}'.format(commune.name,source,len(data)))
 
             dirs = [a for a in glob.glob(basepath + '/*') if os.path.isdir(a)]
             if len(dirs) == 0:
-                warning('{} {} NO DATA DIRECTORY'.format(commune.name,source),log)
+                out.warning('{} {} NO DATA DIRECTORY'.format(commune.name,source))
             for d in dirs:
                 date = datetime.datetime.strptime(d[d.rfind('/')+1:],"%Y-%m-%dT%H-%M-%S")
-                say('{} {} {} OK'.format(commune.name,source,date))
+                out.say('{} {} {} OK'.format(commune.name,source,date))
 
                 file_suffixs = ['aptarment_appraisal_data_{}.json'.format('portali'),
                                 'aptarment_data_{}.json'.format('portali'),
@@ -99,11 +102,11 @@ for region in regions:
                     if not os.path.isfile(filename):
                         tmp = d+'/{}_{}'.format(slugify(commune.name,only_ascii=True),file_suffix)
                         if not os.path.isfile(tmp):
-                            warning('{} {} {} {} NOT FOUND'.format(commune.name,source,date,file_suffix),log)
+                            out.warning('{} {} {} {} NOT FOUND'.format(commune.name,source,date,file_suffix))
                             continue
                         else:
-                            warning('{} {} {} {} FALTA TILDE?'.format(commune.name,source,date,file_suffix),log)
+                            out.warning('{} {} {} {} FALTA TILDE?'.format(commune.name,source,date,file_suffix))
                             filename = tmp
 
                     data = json.load(codecs.open(filename, 'r', 'utf-8-sig'))
-                    say('{} {} {} {} # = {}'.format(commune.name,source,date,file_suffix,len(data)))
+                    out.say('{} {} {} {} # = {}'.format(commune.name,source,date,file_suffix,len(data)))
