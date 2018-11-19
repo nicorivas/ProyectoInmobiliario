0b13c988d23750be77b98bb4da7b181d29b5beac
pabloferre <pablo.ferre@gmail.com>
2018-11-19 16:59:59 -0300

    subiendo

--- web/user/models.py
+++ web/user/models.py
@@ -8,7 +8,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 class UserProfile(models.Model):
-    rut = models.CharField('RUT', max_length=14)
+    rut = models.CharField('RUT', max_length=14, null=True, default='')
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user', primary_key=True)
     first_name = models.CharField(max_length=100, default='')
     last_name = models.CharField(max_length=100, default='')
