e5bc5cae976ae6207cea3186c56b1ad607d7ec8f
pabloferre <pablo.ferre@gmail.com>
2018-11-22 12:40:12 -0300

    tasación palau 1661

--- web/evaluation/views.py
+++ web/evaluation/views.py
@@ -18,48 +18,6 @@ def get_appraisalEvaluations(request):
 
 
 def appraiserEvaluationView(request):
-    evaluationForm = EvaluationForm()
-    if request.method == 'POST':
-        if request.method == 'POST':
-            print(request.POST)
-            if 'delete' in request.POST:
-                # Handle the delete button, next to every appraisal
-                id = int(request.POST['appraisal_id'])
-                appraisal = Appraisal.objects.get(pk=id)
-                appraisal.delete()
-            if 'btn_assign_tasador' in request.POST.keys():
-                print(request.POST.dict())
-                ret = assign_tasadorNF(request)
-            if 'btn_assign_visador' in request.POST.keys():
-                print(request.POST.dict())
-                ret = assign_visadorNF(request)
-            if 'evaluadorAppraisal_id' in request.POST.keys():
-                evaluationForm = EvaluationForm(request.POST)
-                if evaluationForm.is_valid():
-                    evaluationForm.save()
-                    _onTime = evaluationForm.cleaned_data['onTime']
-                    _completeness = evaluationForm.cleaned_data['completeness']
-                    _generalQuality = evaluationForm.cleaned_data['generalQuality']
-                    _commentText = evaluationForm.cleaned_data['commentText']
-                    _commentFeedback = evaluationForm.cleaned_data['commentFeedback']
-                    appraisal = Appraisal.objects.get(pk=request.POST['evaluadorAppraisal_id'])
-                    appraiser = User.objects.get(pk=request.POST['evaluador_id'])
-                    evaluation, created = AppraisalEvaluation.objects.update_or_create(
-                                            appraisal=appraisal,
-                                            #user=appraiser,
-                                            defaults={
-                                                "appraisal":appraisal,
-                                                'user':appraiser,
-                                                'onTime':_onTime,
-                                                'completeness':_completeness,
-                                                'generalQuality':_generalQuality,
-                                                'commentText':_commentText,
-                                                'commentFeedback':_commentFeedback})
-
-                    print(evaluation, created)
-                    print(evaluation.appraisalEvaluationMean)
-
-
     appraisals_active, appraisals_finished = userAppraisals(request)
     for i in appraisals_finished: #Asegurarse de que toda tasación tenga una evaluación
         try:
@@ -67,6 +25,47 @@ def appraiserEvaluationView(request):
         except AppraisalEvaluation.DoesNotExist:
             x = AppraisalEvaluation(appraisal=i)
             x.save()
+
+    evaluationForm = EvaluationForm()
+
+    if request.method == 'POST':
+        print(request.POST)
+        if 'delete' in request.POST:
+            # Handle the delete button, next to every appraisal
+            id = int(request.POST['appraisal_id'])
+            appraisal = Appraisal.objects.get(pk=id)
+            appraisal.delete()
+        if 'btn_assign_tasador' in request.POST.keys():
+            print(request.POST.dict())
+            ret = assign_tasadorNF(request)
+        if 'btn_assign_visador' in request.POST.keys():
+            print(request.POST.dict())
+            ret = assign_visadorNF(request)
+        if 'evaluadorAppraisal_id' in request.POST.keys():
+            evaluationForm = EvaluationForm(request.POST)
+            if evaluationForm.is_valid():
+                _onTime = evaluationForm.cleaned_data['onTime']
+                _completeness = evaluationForm.cleaned_data['completeness']
+                _generalQuality = evaluationForm.cleaned_data['generalQuality']
+                _commentText = evaluationForm.cleaned_data['commentText']
+                _commentFeedback = evaluationForm.cleaned_data['commentFeedback']
+                appraisal = Appraisal.objects.get(pk=request.POST['evaluadorAppraisal_id'])
+                appraiser = User.objects.get(pk=request.POST['evaluador_id'])
+                evaluation, created = AppraisalEvaluation.objects.update_or_create(
+                                        appraisal=appraisal,
+                                        #user=appraiser,
+                                        defaults={
+                                            "appraisal":appraisal,
+                                            'user':appraiser,
+                                            'onTime':_onTime,
+                                            'completeness':_completeness,
+                                            'generalQuality':_generalQuality,
+                                            'commentText':_commentText,
+                                            'commentFeedback':_commentFeedback})
+                #evaluationForm.save()
+                print(evaluation, created)
+                print(evaluation.appraisalEvaluationMean)
+
 
     tasadores = list(User.objects.filter(groups__name__in=['tasador']))
     visadores = list(User.objects.filter(groups__name__in=['visador']))
